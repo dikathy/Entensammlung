6877e2ee9383d56a7e0cf638509cd452bdf99bef
Bernert_K <Bernert_K@DPN-NB-0304>
2016-09-29 15:43:25 +0200

    Projektdateien hinzufügen.

---
+++ Entensammlung/Ente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entensammlung
+{
+    class Ente : IComparable<Ente>
+    {
+        public int Größe;
+        public EntenArt Art;
+
+        public int CompareTo(Ente vergleichsEnte)
+        {
+            if (this.Größe > vergleichsEnte.Größe)
+                return 1;
+            else if (this.Größe < vergleichsEnte.Größe)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
---
+++ Entensammlung/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entensammlung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Ente> enten = new List<Ente>()
+            {
+                new Ente() {Art = EntenArt.Stockente, Größe = 40 },
+                new Ente() {Art = EntenArt.Moschusente, Größe = 42 },
+                new Ente() {Art = EntenArt.Lockente, Größe = 35 },
+                new Ente() {Art = EntenArt.Moschusente, Größe = 28 },
+                new Ente() {Art = EntenArt.Stockente, Größe = 37 },
+                new Ente() {Art = EntenArt.Lockente, Größe = 36 },
+
+            };
+
+            EnteComparerNachGröße ecGröße = new EnteComparerNachGröße();
+            enten.Sort(ecGröße);
+            EntenAusgeben(enten);
+
+            EnteCompareNachArt ecArt = new EnteCompareNachArt();
+            enten.Sort(ecArt);
+            EntenAusgeben(enten);
+
+            Console.ReadKey();
+        }
+
+        public static void EntenAusgeben(List<Ente> enten)
+        {
+            foreach(Ente ente in enten)
+            {
+                Console.WriteLine(ente.Größe.ToString() + "-cm " + ente.Art.ToString());
+            }
+            Console.WriteLine("Enten-Ende!");
+        }
+    }
+}
